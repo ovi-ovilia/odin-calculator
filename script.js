cc854197402ff15d6ed730fb4ae9de3e41a8566b
ovi-ovilia <ol9medina@gmail.com>
2024-03-26 15:59:11 -0300

    debugged operation buttons  improved display and variable logic

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// DUMB ASS DESIGN. THE DOM SHOULDNT INFLUENCE THE RESULTS BUT THE OTHER WAY AROUND
+// MAKE SURE THAT NEXT TIME YOU USE OBJECT AND CALL FUNCTIONS TO THEM INFLUENCE THE DOM
+// FFS IM DUMB BUT AT LEAST IT'S (KIND OF) DONE
+
 const buttons = document.querySelectorAll('.button')
 const visor = document.querySelector('#big-visor')
 const smolVisor = document.querySelector('#smol-visor')
@@ -41,23 +45,32 @@ function buttonInput(id){
         clearButton()}
     else if(id == 'backspace'){
         backSpace()}
+    else if(id == 'float'){
+        float()
+    }
 }
 
 
 function run(a, b ,operation){
-    if(a ==  null){ a = first = parseFloat(visor.innerHTML)}
+    if(visor.innerHTML == ''){return}
+    if(a ==  null){ a = parseFloat(visor.innerHTML)}
     switch (operation){
+        // When a has no value the functions still run. Returning NaN
       case add :
-        add(a, b)
+        add(a, b);
+        operator.innerHTML = '';
         break
       case subtract :
-        subtract(a,b)
+        subtract(a,b);
+        operator.innerHTML = '';
         break
       case multiply :
-        multiply(a,b)
+        multiply(a,b);
+        operator.innerHTML = '';
         break 
       case divide :
         divide(a,b)
+        operator.innerHTML = '';
         break
     }
 }
@@ -65,13 +78,19 @@ function run(a, b ,operation){
 
 function addButton(){
     // Check if it has any prior input. Runs the operation if so
-    if(second != null){
-        first = parseFloat(visor.innerHTML)
-        run(first,second, add)
-    } else{    
+    if(second != null && visor.innerHTML != ''){
+        first = parseFloat(visor.innerHTML);
+        run(first,second, add);
+        operator.innerHTML = '';
+    } else if(second != null && visor.innerHTML == ''){
+        operation = add;
+        operator.innerHTML = '+';
+        first = null
+    }else{    
         second = parseFloat(visor.innerHTML)
         smolVisor.innerHTML = second;
-        visor.innerHTML = '';
+        visor.innerHTML = ''
+        operator.innerHTML = '+';
         operation = add;
     }
 }
@@ -85,16 +104,26 @@ function add(a,b){
 }
 
 function subtractButton(){
-    if(second != null){
-        first = parseFloat(visor.innerHTML)
-        run(first,second, subtract)
-    } else{    
-        second = parseFloat(visor.innerHTML)
-        smolVisor.innerHTML = second;
-        visor.innerHTML = '';
-        operation = subtract;
+        if(second != null && visor.innerHTML != ''){
+
+            first = parseFloat(visor.innerHTML);
+            run(first,second, subtract);
+            operator.innerHTML = '';
+        } else if(second != null && visor.innerHTML == ''){
+
+            operation = subtract;
+            operator.innerHTML = '-';
+            first = null;
+        }else{    
+
+            second = parseFloat(visor.innerHTML)
+            smolVisor.innerHTML = second;
+            visor.innerHTML = ''
+            operator.innerHTML = '-';
+            operation = subtract;
+        }
     }
-}
+
 function subtract(a,b){
     let diff = b - a;
     visor.innerHTML = '';
@@ -104,14 +133,20 @@ function subtract(a,b){
 }
 
 function multiplyButton(){
-    if(second != null){
-        first = parseFloat(visor.innerHTML)
-        run(first,second, multiply)
-    } else{    
+    if(second != null && visor.innerHTML != ''){
+        first = parseFloat(visor.innerHTML);
+        run(first,second, multiply);
+        operator.innerHTML = '';
+    } else if(second != null && visor.innerHTML == ''){
+        operation = multiply;
+        operator.innerHTML = 'x';
+        first = null;
+    }else{    
         second = parseFloat(visor.innerHTML)
         smolVisor.innerHTML = second;
-        visor.innerHTML = '';
-        operation = multiply;
+        visor.innerHTML = ''
+        operator.innerHTML = 'x';
+        operation = multipply;
     }
 }
 
@@ -126,24 +161,31 @@ function multiply(a,b){
 
 
 function divideButton(){
-    if(second != null){
-        first = parseFloat(visor.innerHTML)
-        run(first,second, divide)
-    } else{    
+    if(second != null && visor.innerHTML != ''){
+        first = parseFloat(visor.innerHTML);
+        run(first,second, divide);
+        operator.innerHTML = '';
+    } else if(second != null && visor.innerHTML == ''){
+        operation = divide;
+        operator.innerHTML = '/';
+        first = null;
+    }else{    
         second = parseFloat(visor.innerHTML)
         smolVisor.innerHTML = second;
-        visor.innerHTML = '';
+        visor.innerHTML = ''
+        operator.innerHTML = '/';
         operation = divide;
     }
 }
 
 
 function divide(a,b){
+    if(a == 0){alert('haha!! You cannot divide by 0'), clearButton(); return}
     let product = b/a;
     visor.innerHTML = '';
-    second = product;
+    second = Number(product.toFixed(2));
     operation = null;
-    smolVisor.innerHTML = product; 
+    smolVisor.innerHTML = Number(product.toFixed(2)); 
 }
 
 function clearButton(){
@@ -152,11 +194,18 @@ function clearButton(){
     operation = null;
     smolVisor.innerHTML = '';
     visor.innerHTML = '';
+    operator.innerHTML = '';
 }
 
 function backSpace(){
     let toBackspace = visor.innerHTML;
     visor.innerHTML = toBackspace.replace(toBackspace[toBackspace.length -1], '')
+}
+
+function float(){
+    let x = visor.innerHTML;
+    visor.innerHTML = Number.parseFloat(x).toFixed(0);
+    
 }
 
 
